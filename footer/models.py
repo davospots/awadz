dbb476c84fe3707cbf787d7de023cbcbe5c3368e
Dave <david.mathaga@student.moringaschool.com>
2021-12-15 09:16:45 +0300

    footer

--- footer/models.py
+++ footer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+	full_name = models.CharField(max_length=30)
+	email = models.EmailField()
+	message = models.TextField()
+	date = models.DateTimeField(default=timezone.now)
+	
+	def __str__(self):
+		return self.full_name
+
+
+class Quote(models.Model):
+	quote= models.TextField()
+	date = models.DateTimeField(default=timezone.now)
+	def __str__(self):
+		return self.quote
